65c361b1fb1994d94e2c07f9a6c28ef6f913255c
akshay <akshaymurali8424@gmail.com>
2023-05-05 18:31:02 +0530

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>TODO APP USING VUE</h1>
+    <div>
+      <input
+        type="text"
+        placeholder="type something"
+        class="my-input"
+        v-model="toDo"
+      />
+      <button class="my-button" @click="addToDo(toDo)">add</button>
+    </div>
+    <div>
+        <ToDoCard  v-for="(element,i) in toDos" :to-dos="toDos"  :key="i"  :todo="element" :index="i"/>  
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoCard from './ToDoCard.vue';
+
+export default{
+    name: "Home",
+    data(){
+        return{
+            toDo:'',
+            toDos:[]
+        }
+    },
+    methods:{
+        addToDo(toDo){
+            if(toDo.trim()){
+                this.toDos.push(toDo) 
+                this.toDo=''
+            }
+        },
+    },
+    components: { ToDoCard }
+}
+</script>
+
+<style >
+ .my-input {
+  padding: 12px;
+  border-radius: 5px;
+  border: none;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
+
+.my-button {
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #2c3e50;
+  color: #fff;
+  font-weight: 400;
+  border: none;
+} 
+
+
+</style>
---
+++ src/components/ToDoCard.vue
@@ -0,0 +1,48 @@
+<template>
+
+<div class="card">
+  <div>
+    {{ todo }}
+  </div>
+  <div>
+    <button @click="removeToDo(index)" class="my-button">done</button>
+  </div>
+</div>
+
+</template>
+
+<script>
+  export default {
+    name:"toDoCard",
+    props:{
+        todo: {type:String,required:true},
+        index:{type:Number,required:true},
+        toDos:{type:Array,required:true}
+    },
+    methods:{
+      removeToDo(index){
+              this.toDos.splice(index,1)
+      }
+    }
+  }
+</script>
+
+
+<style>
+
+.card{
+  display: flex;
+  justify-content: space-between;
+  padding: 12px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  width: 20%;
+  border-radius: 12px;
+  margin: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
+
+</style>
+
